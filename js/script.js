6fe726630305347d66f8a25b0373f0017abccd51
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-01-28 17:17:38 +0200

    eslint

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Тут вийшло витягнути з масиву кількість парних чисел :-)
+// const numbers = [1, 3, 5, 7, 8, 9, 12, 15, 18, 32, 44, 63];
+// let total = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) {
+//     total += 1;
+//   } else if (numbers[i] !== 0) {
+//     continue;
+//   }
+// }
+// console.log(total);
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   let total = 1;
+//   for (const lengthArr of message) {
+//     total *= lengthArr;
+//   }
+//   return total;
+// }
+
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 20));
+// console.log(calculateEngravingPrice('Web-development is creative work', 40));
+
+// const name = "Mango";
+// console.log(name.split("")); // ["M", "a", "n", "g", "o"]
+
+// const message = "JavaScript essentials";
+// console.log(message.split(" ")); // ["JavaScript", "essentials"]
