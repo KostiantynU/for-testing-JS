44e1fc4efdf3ac75e42949d79c776f7688e9d1ed
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-02-01 08:36:57 +0200

    looking for biggest length of word

--- js/script.js
+++ js/script.js
@@ -4,21 +4,26 @@ function findLongestWord(string) {
   let letters = string.split(' ');
   console.log('Look at massive: ', letters);
   let leng = 0;
-  let biggest = letters[0].length;
+  console.log('Length of zero index: ', leng);
+  let biggest = '';
+  let temporary = letters[0].length;
   for (let i = 0; i < letters.length; i += 1) {
-    leng = [letters[i].length];
+    leng = letters[i].length;
     console.log('Length of item: ', letters[i], ' ', leng);
-    if (biggest > leng) {
-      biggest = leng;
+    if (temporary < leng) {
+      biggest = letters[i];
       console.log('This is it?: ', biggest);
     }
+    console.log('The biggest is: ', biggest);
   }
-
-  // Change code above this line
+  return biggest;
 }
 
-findLongestWord('The quick brown fox jumped over the lazy dog');
-findLongestWord('Google do a roll');
+// Change code above this line
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('May the force be with you'));
 
 // const array1 = [5, 10, 15, 20];
 // const array2 = [10, 20, 30];
