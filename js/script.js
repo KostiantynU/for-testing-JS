22827dabaad4d35f95d9ecd5c799af4d63c5e023
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-02-02 23:31:02 +0200

    find two equals numbers in two dofferents arrays

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
+function getCommonElements(array1, array2) {
   // Change code below this line
-  let temporary = 0;
-  for (let i = min; i < max; i += 1) {
-    temporary += numbers[i]; // Here i need to use method, that will add my number after zero index ;-)
-    console.log(numbers);
+  let totalArr = [];
+  for (const number of array1) {
+    // console.group('Before if: ', number);
+    // console.log('Before if totalArr: ', totalArr);
+    if (array1.includes(number) && array2.includes(number)) {
+      totalArr.push(number);
+      console.log('In if totalArr: ', totalArr);
+    }
+
+    // } else {
+    //   for (const numberr of array2) {
+    //     console.group('Before if array2: ', numberr);
+    //     console.log('Before if array2 totalArr: ', totalArr);
+    //     if (array1.includes(numberr) && array2.includes(numberr)) {
+    //       totalArr.push(numberr);
+    //       console.log('In if  array2 totalArr: ', totalArr);
+    //     }
+    //   }
+    // }
   }
+  return totalArr;
+  // if (array1.includes(i)  array2.includes(i)) {
+  //   myArray1 = array1[i];
+  //   console.log('Look in number: ', array1[i]);
+  //   console.log('My myArr1: ', myArray1);
+  // }
+
+  // for (let i = 0; i < array2.length; i += 1) {
+  //   console.log(array2.includes(2));
+  //   console.log('Number of iteration: ', i);
+  //   if (array2.includes(2) === array1.includes(3)) {
+  //     myArray2 = array2[i];
+  //     console.log('Look in number: ', array2[i]);
+  //     console.log('My myArr2: ', myArray2);
+  //   }
+  // }
   // Change code above this line
-  return numbers;
 }
-console.log(createArrayOfNumbers(1, 3));
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
--- js/work-with-massives-numbers.js
+++ js/work-with-massives-numbers.js
@@ -137,3 +137,77 @@
 //   total = +number;
 // }
 // console.log(total);
+
+// ---------- Example 22/32, add in arrow numbers from min to max ----------
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   let temporary = 0;
+//   for (let i = min; i <= max; i += 1) {
+//     numbers.push(i); // Here i need to use method, that will add my number after zero index ;-)
+//     console.log(numbers);
+//   }
+//   // Change code above this line
+//   return numbers;
+// }
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+
+// ---------- Example 23/32, it's wants that i make new array from numbers, what bigger then value
+// function filterArray(numbers, value) {
+//    let totalArr = [];
+//    for (const number of numbers) {
+//     if (number > value) {
+//       totalArr.push(number);
+//     }
+//   }
+//   return totalArr;
+// }
+
+// ---------- Example 25/32, it was haaard, hard to fine two equale numbers in two different arrays
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   let totalArr = [];
+//   for (const number of array1) {
+//     // console.group('Before if: ', number);
+//     // console.log('Before if totalArr: ', totalArr);
+//     if (array1.includes(number) && array2.includes(number)) {
+//       totalArr.push(number);
+//       console.log('In if totalArr: ', totalArr);
+//     }
+
+//     // } else {
+//     //   for (const numberr of array2) {
+//     //     console.group('Before if array2: ', numberr);
+//     //     console.log('Before if array2 totalArr: ', totalArr);
+//     //     if (array1.includes(numberr) && array2.includes(numberr)) {
+//     //       totalArr.push(numberr);
+//     //       console.log('In if  array2 totalArr: ', totalArr);
+//     //     }
+//     //   }
+//     // }
+//   }
+//   return totalArr;
+//   // if (array1.includes(i)  array2.includes(i)) {
+//   //   myArray1 = array1[i];
+//   //   console.log('Look in number: ', array1[i]);
+//   //   console.log('My myArr1: ', myArray1);
+//   // }
+
+//   // for (let i = 0; i < array2.length; i += 1) {
+//   //   console.log(array2.includes(2));
+//   //   console.log('Number of iteration: ', i);
+//   //   if (array2.includes(2) === array1.includes(3)) {
+//   //     myArray2 = array2[i];
+//   //     console.log('Look in number: ', array2[i]);
+//   //     console.log('My myArr2: ', myArray2);
+//   //   }
+//   // }
+//   // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
