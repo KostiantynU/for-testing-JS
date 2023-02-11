c579c29a8ef898478770f237d139ffe0312a96f8
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-02-11 09:54:22 +0200

    objects

--- js/homeTasksObj.js
+++ js/homeTasksObj.js
@@ -1,8 +1,24 @@
-function multiply(firstNumber, secondNumber, ...otherArgs) {
-  console.log(firstNumber);
-  console.log(secondNumber);
-  console.log(otherArgs);
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+  console.log('in fn', productName);
+  for (const product of products) {
+    console.log('in cycel', product.name);
+    if (product.name === productName) {
+      console.log('this is true');
+    }
+  }
+  return null;
+  // Change code above this line
 }
-multiply(1, 2);
-multiply(1, 2, 3);
-multiply(1, 2, 3, 4, 5);
+getProductPrice('Radar');
+getProductPrice('Grip');
+getProductPrice('Scanner');
+getProductPrice('Droid');
+getProductPrice('Engine');
--- js/objects-array.js
+++ js/objects-array.js
@@ -39,3 +39,46 @@
 // const avarageRating = (totalRating / books.length).toFixed(1);
 // console.log('Look the avarage:', avarageRating);
 // console.log('length: ', books.length - 1);
+
+// --------- Task with joke - count the array without FOR OF ;-)
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const arrayKeys = Object.keys(object);
+//   propCount = arrayKeys.length;
+//   // console.log('arrayKeys', arrayKeys);
+//   // const destrArray = [...arrayKeys];
+//   // console.log('destrArray', destrArray);
+//   // for (const key in object) {
+
+//   // }
+
+//   return propCount;
+//   // Change code above this line
+// }
+// const big = { name: 'Big', age: 45 };
+// const bigger = Object.create(big);
+// bigger.year = 1985;
+// console.log(bigger);
+// console.log('bigger in countProps', countProps({ bigger }));
+// console.log(countProps({ name: 'Mango', age: 2 }));
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+// arrayKeys.hasOwnProperty(key) ? (propCount += 1) : console.log('Not his values');
+
+// ---------- 03:17 Count objects from array, get values of object in another arrays
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.log('Here looking in HEX: ', hexColors);
+// console.log('Here is rgbColors: ', rgbColors);
