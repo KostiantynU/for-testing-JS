e12a1b97322a969108548a3f34d3514da5c5dc0d
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-02-17 21:57:31 +0200

    Update homeTasksObj.js

--- js/homeTasksObj.js
+++ js/homeTasksObj.js
@@ -1,16 +1,56 @@
-function changeEven(numbers, value) {
-  // Change code below this line
-  const newArray = [...numbers];
-
-  newArray.forEach(element => {
-    if (element % 2 === 0) {
-      let number = element + value;
-      let indexFor = newArray.indexOf(element);
-      newArray.splice(indexFor, 1, number);
+//  * Працюємо з колекцією товарів у кошику:
+//  * - getItems()
+//  * - add(product)
+//  * - remove(productName)
+//  * - clear()
+//  * - countTotalPrice()
+//  * - increaseQuantity(productName)
+//  * - decreaseQuantity(productName)
+//  *
+//  */
+const basket = {
+  items: [],
+  getItems() {
+    return this.items;
+  },
+  add(product) {
+    this.items.push(product);
+    console.log(this.items);
+  },
+  remove(productName) {
+    for (const names of this.items) {
+      let indexOfNames = this.items.indexOf(names);
+      if (names.name === productName) {
+        this.items.splice(indexOfNames, 1);
+      }
     }
-  });
-  return newArray;
-  // Change code above this line
-}
-console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
-console.log(changeEven([17, 24, 68, 31, 42], 100));
+    console.log(this.items);
+  },
+  clear() {
+    this.items = [];
+    return this.items;
+  },
+  countTotalPrice() {
+    let totalPrice = 0;
+    for (const numbers of this.items) {
+      console.log('price:', numbers.price);
+      totalPrice += numbers.price;
+      console.log(totalPrice);
+    }
+  },
+};
+const fruits = [
+  { name: '🍎', price: 50 },
+  { name: '🍇', price: 70 },
+  { name: '🍋', price: 60 },
+  { name: '🍓', price: 110 },
+];
+console.log(basket.getItems());
+basket.add(fruits[0]);
+basket.add(fruits[1]);
+basket.add(fruits[2]);
+basket.add(fruits[3]);
+console.log(basket.items);
+// console.log(basket.remove('🍎'));
+// console.log(basket.clear());
+console.log(basket.countTotalPrice());
