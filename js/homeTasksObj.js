ec67b4f9b89c4a4c1fed271ac92dc41fc7f42e3a
KostiantynU <umaneck@gmail.com>
2023-02-21 16:17:32 +0200

    fignia

--- js/homeTasksObj.js
+++ js/homeTasksObj.js
@@ -24,9 +24,14 @@
 // const bookByAuthor = books.find(book => book.author === AUTHOR);
 // console.log(bookWithTitle);
 // console.log(bookByAuthor);
-const person = {
-  firstName: 'John',
-  age: 30,
-};
-const { firstName: personName } = person;
-console.log(personName);
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+
+console.log(colorPickerOptions.findIndex(option => option.label === "blue"));
+console.log(colorPickerOptions.findIndex(option => option.label === "pink"))
+console.log(colorPickerOptions.findIndex(option => option.label === "white"));
