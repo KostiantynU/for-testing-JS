8bdcf9a113d90292d1ade907a972c6510b6750b6
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-02-25 05:47:35 +0200

    work with sort

--- js/sort-arrays.js
+++ js/sort-arrays.js
@@ -28,19 +28,145 @@
 // // console.log(inDescendingScoreOrder);
 // console.log(inAlphabeticalOrder);
 
-const students = [
-  { name: 'Манго', score: 83, courses: ['математика', 'фізика'] },
-  { name: 'Полі', score: 59, courses: ['інформатика', 'математика'] },
-  { name: 'Аякс', score: 37, courses: ['фізика', 'біологія'] },
-  { name: 'Ківі', score: 94, courses: ['література', 'інформатика'] },
-];
+// const students = [
+//   { name: 'Манго', score: 83, courses: ['математика', 'фізика'] },
+//   { name: 'Полі', score: 59, courses: ['інформатика', 'математика'] },
+//   { name: 'Аякс', score: 37, courses: ['фізика', 'біологія'] },
+//   { name: 'Ківі', score: 94, courses: ['література', 'інформатика'] },
+// ];
 
-// const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
-// console.log('AscendingScore', sortedByAscendingScore);
-// const names = [...students].sort((a, b) => a.score - b.score).map(student => student.name);
-// console.log('names:', names);
-const uniqueSortedCourses = students
-  .flatMap(student => student.courses)
-  .filter((course, index, array) => array.indexOf(course) === index)
-  .sort((a, b) => a.localeCompare(b));
-console.log('uniqueSortedCourses:', uniqueSortedCourses);
+// // const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
+// // console.log('AscendingScore', sortedByAscendingScore);
+// // const names = [...students].sort((a, b) => a.score - b.score).map(student => student.name);
+// // console.log('names:', names);
+// const uniqueSortedCourses = students
+//   .flatMap(student => student.courses)
+//   .filter((course, index, array) => array.indexOf(course) === index)
+//   .sort((a, b) => a.localeCompare(b));
+// console.log('uniqueSortedCourses:', uniqueSortedCourses);
+
+// -----------04:39 simple sort
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+// ----------04:40 sort array
+// const authors = [
+//   'Tanith Lee',
+//   'Bernard Cornwell',
+//   'Robert Sheckley',
+//   'Fyodor Dostoevsky',
+//   'Howard Lovecraft',
+// ];
+// // Change code below this line
+
+// const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+
+// const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
+// console.log(authorsInAlphabetOrder);
+// console.log(authorsInReversedOrder);
+
+// ---------04:41 sort array with objects
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const sortedByAuthorName = [...books].sort((a, b) => a.author.localeCompare(b.author));
+
+// const sortedByReversedAuthorName = [...books].sort((a, b) => b.author.localeCompare(a.author));
+
+// const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+
+// const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+
+// ---------04:42 sort by balance
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+// // Change code below this line
+// const sortByAscendingBalance = users => {
+//    return [...users].sort((a, b) => a.balance - b.balance);
+// };
+// // Change code above this line
