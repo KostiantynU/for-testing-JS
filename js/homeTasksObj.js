dccfe84646cfd64f33f5bb67c465443e5d4bbadf
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-03-04 13:52:48 +0200

    work with obj

--- js/homeTasksObj.js
+++ js/homeTasksObj.js
@@ -1,9 +1,19 @@
-const showThis = () => {
-  console.log(this);
-};
-showThis();
-const user = {
-  name: 'mango',
-};
-user.showContext = showThis;
-user.showContext();
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+// const mango = new User('Mango', 'mango@gmail.com');
+// console.log(mango);
+// const poly = new User('Poly', 'poly@yahoo.com');
+// console.log(poly);
+const array = [5, 10, 15, 20];
+console.log(
+  array.forEach(function () {
+    console.log(this.a);
+    {
+      a: test;
+    }
+  })
+);
